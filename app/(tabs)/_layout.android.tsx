bb4f231329368c9590e589c8337971ae7eb08820
vedant <vedant@gmail.com>
2026-06-15 15:15:20 +0530

    feat: implement android tab layout and reusable ScreenWrapper component

--- app/(tabs)/_layout.android.tsx
+++ app/(tabs)/_layout.android.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { NativeTabs, Icon, Label } from "expo-router/unstable-native-tabs";
+import * as NavigationBar from "expo-navigation-bar";
 
 import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
@@ -7,11 +8,17 @@ import { useColorScheme } from "@/hooks/use-color-scheme";
 export default function TabLayout() {
     const colorScheme = useColorScheme();
     const colors = Colors[colorScheme ?? "light"];
+    const tabBarColor = "#161616";
+
+    useEffect(() => {
+        NavigationBar.setButtonStyleAsync("light");
+    }, []);
 
     return (
         <NativeTabs
             tintColor={colors.tint}
             disableTransparentOnScrollEdge
+            backgroundColor={tabBarColor}
         >
             <NativeTabs.Trigger name="index">
                 <Label>Home</Label>
